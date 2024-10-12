311fb9b34634e42e1700c66583ce39dc6458aeaa
pecix <pecixx@gmail.com>
2024-10-12 18:21:25 +0200

    my first running application

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int inputNumber = scanner.nextInt();
+
+        System.out.println("Podana liczba to: " + inputNumber);
+
     }
 }
