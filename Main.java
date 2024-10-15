90afe80a987941f83bfb9be5567cff0509858ead
pecix <pecixx@gmail.com>
2024-10-15 22:12:04 +0200

    add gitignore

--- Main.java
+++ Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbę: ");
-        int inputNumber = scanner.nextInt();
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Podaj liczbę: ");
+       int inputNumber = scanner.nextInt();
 
-        System.out.println("Podana liczba to: " + inputNumber);
+       System.out.println("Podana liczba to: " + inputNumber);
 
+        // for (int i = 0; i < 10; i++) {
+            // System.out.print(i + ", ");
+        // }
     }
 }
